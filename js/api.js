c6122446b9b8b7ec39c7e62cf44a72f40c1bdf91
findshen <sx0523@gmail.com>
2021-10-09 09:26:20 +0800

    123

--- js/api.js
+++ js/api.js
@@ -1,6 +1,7 @@
 
-var apiUrl="http://129.204.3.98:8021";
-// var apiUrl="http://localhost:8021";
+// var apiUrl="http://129.204.3.98:8021";
+// var apiUrl="http://10.100.6.16:8021";
+var apiUrl="http://localhost:8021";
 
 
 
