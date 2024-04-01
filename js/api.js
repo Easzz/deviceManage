06b8e078bcedc229d879880ee9bf9dd06debeefe
findshen <sx0523@gmail.com>
2024-04-01 20:21:56 +0800

    123

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
 
 // var apiUrl="http://129.204.3.98:8021";
+//var apiUrl="http://10.100.104.36:8021";
 var apiUrl="http://localhost:8021";
 
 
@@ -7,4 +8,4 @@ var apiUrl="http://localhost:8021";
 
 // var fileServerUrl="http://129.204.3.98:8888";
 // var apiUrl="http://localhost:8021";
-var fileServerUrl="http://localhost:8888";
+var fileServerUrl="http://10.100.104.36:8889";
